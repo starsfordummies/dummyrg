a54a63d78e054a25047a47e3281f6eeb1e0532c7
Stefano <stefano.carignano@fqa.ub.edu>
2022-07-25 19:00:27 +0200

    more reordering

---
+++ code/basic_playarounds/example_main.py
@@ -0,0 +1,21 @@
+# a main example 
+
+
+def myfunc(): # looks good to me 
+
+    import numpy as np
+    from tensornetwork import ncon
+    import logging
+
+    import copy 
+    import matplotlib.pyplot as plt
+
+
+    logging.basicConfig()
+    logger = logging.getLogger('Something')
+    logger.setLevel(logging.DEBUG)
+
+
+
+if __name__ == "__main__":
+    myfunc()
