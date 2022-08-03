7cd8a329bbe830e75b4fd5a160bd2ddf07149129
Stefano <stefano.carignano@fqa.ub.edu>
2022-08-03 17:39:50 +0200

    added check for tensor eq. after transposing

--- code/stefanoMPS/matrixUtils.py
+++ code/stefanoMPS/matrixUtils.py
@@ -18,4 +18,73 @@ def checkIdMatrix(ainp: np.array, epstol = 1e-14) -> bool:
 
 # TODO:
 def myReshape(idx: dict, tensor: np.array):
-    np.reshape(tensor) #...
+    pass
+
+def alleq(tensor1: np.array, tensor2:np.array) -> bool:
+  if np.shape(tensor1) != np.shape(tensor2): return False
+
+  return np.ravel(np.isclose(tensor1,tensor2)).all()
+
+def build_perms(tensor: np.array, n: int, perm: tuple, listPerms: list) -> list:
+    
+    outList = listPerms
+
+    if n == 2:
+      #print(f"doing last two with input {perm}")
+      outList.append(perm)
+      last = perm[:]
+      last[-2], last[-1] = last[-1], last[-2]
+      outList.append(last)
+
+    else: # n > 2
+      #print(perm)
+      #print(f"Starting from {perm[-n:]}")
+      for i, si in enumerate(perm[-n:]):
+          #print(i, si)
+          tperm = perm[:]
+          tperm[-n], tperm[-n+i] = tperm[-n+i], tperm[-n]
+          #print(f"Passing {tperm} to {n-1} buildperms")
+
+          build_perms(tensor, n-1, tperm, outList)
+
+          #print(f"output: {outList}")
+
+    return outList
+          
+
+      #perm(tensor,n-1)
+    
+
+
+def equal_up_to_perm(t1: np.array, t2: np.array) -> bool:
+  # First check if shapes are different
+  s1 = np.shape(t1)
+  s2 = np.shape(t2)
+  print(s1,s2)
+  if sum(s1) != sum(s2):
+    print("Cannot possibly reshape one into another, quitting")
+    return False
+  else:
+    print(f"Shapes = {s1}, {s2}, checking..")
+
+  # if shapes are different, it should give a hint on how to reshape..
+
+  if alleq(t1,t2):
+    print("Arrays are equal")
+    return True 
+
+
+""" Example usage: 
+
+  c0 = [i for i in range(0,len(s2))]
+  lps = []
+  build_perms(t2, len(s2), c0, lps)
+
+  print(lps)
+
+  for shuf in lps:
+    if alleq(t1,t2.transpose(shuf)):
+        print(f"Equal after reshuffling {shuf}")
+        return True
+
+"""
