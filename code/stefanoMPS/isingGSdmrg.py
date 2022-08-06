511613d56b582c112ea74615e4df06b405115055
Stefano <stefano.carignano@fqa.ub.edu>
2022-08-06 15:48:20 +0200

    updates to DMRG, still not quite right

--- code/stefanoMPS/isingGSdmrg.py
+++ code/stefanoMPS/isingGSdmrg.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import myMPSstuff as mps
 import myMPOstuff as mpo
+import applMPOMPS as mpomps
 
 import myDMRG as dmrg
 
@@ -17,17 +18,18 @@ chiM = 40
 
 gg = 0.4
 
-"""
+
 # Do it with tenpy 
 E_tenpy, psi_tenpy, _ = example_DMRG_tf_ising_finite(LLL, gg, chiM)
 Smid_tenpy = psi_tenpy.entanglement_entropy()[(LLL-1)//2]
-"""
 
 
-psi = mps.myMPS(mps.randMPS(LLL))
+
+psi = mps.myMPS(mps.randMPS(LLL,chi=20))
 
 Hising = mpo.myMPO(IsingMPO(LLL, J=1., g=gg))
 
 Emin = dmrg.findGS_DMRG(Hising, psi)
 
+Emin = mpomps.expValMPO(psi, Hising)
 print(Emin)
--- code/stefanoMPS/myDMRG.py
+++ code/stefanoMPS/myDMRG.py
@@ -28,13 +28,14 @@ def findGS_DMRG( inMPO : mpo.myMPO, inMPS: mps.myMPS) -> mps.myMPS:
     Apsi = inMPS.MPS
     dd = inMPO.DD
 
-    nsweeps = 20
+    nsweeps = 10
 
     toleig = 1e-6
     for ns in range(0,nsweeps):
+
         toleig = toleig*0.1
       
-        # L->R sweep 
+        # ===>>===>     (L-R sweep )
         for jj, Aj in enumerate(Apsi[:-1]):
 
             Heff = ncon([le[jj], ww[jj], ww[jj+1], re[jj+2]],
@@ -42,10 +43,10 @@ def findGS_DMRG( inMPO : mpo.myMPO, inMPS: mps.myMPS) -> mps.myMPS:
 
             Heff = Heff.reshape( chis[jj]*dd*dd*chis[jj+2], chis[jj]*dd*dd*chis[jj+2])
 
-            lam0, eiv0 = LAS.eigsh(Heff, k=1, which='SA',tol=toleig) #, v0=psi_flat, tol=tol, ncv=N_min)
-
-            u, s, vdag, chiTrunc = mps.SVD_trunc(eiv0.reshape(chis[jj]*dd,dd*chis[jj+2]),1e-12,40)
-
+            lam0, eivec0 = LAS.eigsh(Heff, k=1, which='SA',tol=toleig) #, v0=psi_flat, tol=tol, ncv=N_min)
+            print(f"{lam0 = }")
+            u, s, vdag, chiTrunc = mps.SVD_trunc(eivec0.reshape(chis[jj]*dd,dd*chis[jj+2]),1e-14,40)
+            print(f"[L] truncating {chis[jj]*dd} {dd*chis[jj+2]} -> {chiTrunc}")
             ss = LA.sqrtm(np.diag(s))
 
             uss = (u @ ss).reshape(chis[jj],chiTrunc,dd)
@@ -56,11 +57,15 @@ def findGS_DMRG( inMPO : mpo.myMPO, inMPS: mps.myMPS) -> mps.myMPS:
 
             chis[jj+1] = chiTrunc
 
+            #print(f"truncated {np.shape(Apsi[jj])}, {np.shape(Apsi[jj+1])}")
+
+
             # rebuild left env #TODO: do update instead of full rebuild
             le = envs.build_left_env(inMPS,inMPO)
+ 
+        print( [np.shape(i) for i in le])
 
-
-    # R->L sweep 
+        # <====<<====    (R-L sweep)
         Emin = 0.
         for jj, Bj in enumerate(Apsi[1:]):
             rjj = -jj-1
@@ -69,12 +74,14 @@ def findGS_DMRG( inMPO : mpo.myMPO, inMPS: mps.myMPS) -> mps.myMPS:
             [[-1,1,-5],[1,2,-2,-6],[2,3,-3,-7],[-4,3,-8]])
             
             Heff = Heff.reshape( chis[rjj-2]*dd*dd*chis[rjj], chis[rjj-2]*dd*dd*chis[rjj])
-            lam0, eiv0 = LAS.eigsh(Heff, k=1, which='SA',tol=toleig) #, v0=psi_flat, tol=tol, ncv=N_min)
-            u, s, vdag, chiTrunc = mps.SVD_trunc(eiv0.reshape(chis[rjj-2]*dd,dd*chis[rjj]),1e-12,40)
+            lam0, eivec0 = LAS.eigsh(Heff, k=1, which='SA',tol=toleig) #, v0=psi_flat, tol=tol, ncv=N_min)
+            u, s, vdag, chiTrunc = mps.SVD_trunc(eivec0.reshape(chis[rjj-2]*dd,dd*chis[rjj]),1e-14,40)
+            print(f"[R] truncating {chis[rjj-2]*dd} {dd*chis[rjj]} -> {chiTrunc}")
             ss = LA.sqrtm(np.diag(s))
 
             uss = (u @ ss).reshape(chis[rjj-2],chiTrunc,dd)
             ssv = (ss @ vdag).reshape(chiTrunc,chis[rjj],dd) 
+
 
             Apsi[rjj-1] = uss
             Apsi[rjj] = ssv
@@ -84,8 +91,10 @@ def findGS_DMRG( inMPO : mpo.myMPO, inMPS: mps.myMPS) -> mps.myMPS:
 
             # rebuild right env #TODO: do update instead of full rebuild
             re = envs.build_right_env(inMPS,inMPO)
-            Emin = Emin + lam0
+
+            Emin = lam0
 
         print(f"Nsweep = {ns}, En = {Emin}")
+        print(f"chis = {inMPS.chis}")
 
     return Emin 
